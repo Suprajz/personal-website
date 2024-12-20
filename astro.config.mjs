245c2124a58afcd6eac1cd6e225ce6c6c32bea64
Nenad Zivanovic <nenad.zivanovic@codebehind.rs>
2024-12-20 19:09:12 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import icon from "astro-icon";
 export default defineConfig({
   integrations: [icon()],
   site: "https://suprajz.github.io",
-  base: "personal-website",
+  base: "/personal-website/",
 });
